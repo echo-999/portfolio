4454947dbccba5f878c10db11d66d8eeae4fb8ca
echo-999 <60455353+echo-999@users.noreply.github.com>
2021-08-10 19:14:32 +0800

    skills done

--- src/pages/components/SkillCard.js
+++ src/pages/components/SkillCard.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
 export default function SkillCard(props) {
-  const skillsArr = [
-    {title: 'HTML', description: 'HyperText Markup Language'},
-    {title: 'CSS', description: 'Cascading Style Sheets'},
-    {title: 'JS', description: 'JavaScript Language'},
-    {title: 'Material - UI', description: 'A Popular React UI Framework'},
-    {title: 'Tailwind CSS', description: 'A Utility-First CSS Framework'},
-    {title: 'React JS', description: 'A Front-End JavaScript Library'}
-  ]
+  const { title, description, proficiency, level } = props;
   return (
-    <>
-      {skillsArr.map((item, index) => (
-        <div>
-          <h1>{item.title}</h1>
-        </div>
-        
-      ))}
-    </>
+    <div className='border-2 border-cxred rounded-3xl shadow-cx-dark transform rotate-12 hover:rotate-0  bg-cxgreen-light flex flex-col flex-wrap gap-2 px-8 py-2 m-2 items-center'>
+      <p className='text-3xl text-cxdark font-semibold'>{title}</p>
+      <p className='mb-8 text-cxdark text-lg'>{description}</p>
+      <ProficiencyBar level={level} />
+      <p className='italic text-sm mb-1'>{proficiency}</p>
+    </div>
+  )
+}
+
+const ProficiencyBar = ({ level }) => {
+  return (
+    <div className='grid grid-cols-3 gap-x-1 w-full justify-evenly mb-8'>
+      <div className={`bg-cxgreen-dark rounded-3xl border border-cxdark h-6`}></div>
+      <div className={`${level > 1 ? 'bg-cxgreen-dark bg-opacity-80' : 'bg-gray-400'} rounded-3xl border border-cxdark`}></div>
+      <div className={`${level === 3 ? 'bg-cxgreen-dark bg-opacity-75' : 'bg-gray-400'} rounded-3xl border border-cxdark`}></div>
+    </div>
   )
 }
